c6a6a955d62ccd2638a366594cced5cbf110ee36
Akira Noda <akira.noda@onecareer.com>
2024-03-08 14:22:00 +0900

    not install with this command

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ sudo ln -s "$DOTFILES/fish" "$HOME/.config/fish"
 
 rm -rf "$HOME/.config/helix"
 sudo ln -s "$DOTFILES/helix" "$HOME/.config/helix"
-sudo ln -Ts $HOME/helix/runtime "$HOME/.config/helix/runtime"
 
 rm -rf "$HOME/.config/gh"
 sudo ln -s "$DOTFILES/gh" "$HOME/.config/gh"
